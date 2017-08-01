dbee49eed95af6a052a5d762ffe8156b7382f1f9
Todd Loisel <todd@perfectsensedigital.com>
2017-08-01 16:46:59 -0400

    Allow Docker Deploy Script to increment minor version from existing Docker tags

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -2,16 +2,6 @@
 
 function build_container() {
 
-    docker_registry_host=`cat DOCKER_METADATA | grep "DOCKER_REGISTRY_HOST" | awk '{print $2}'`
-    docker_repository=`cat DOCKER_METADATA | grep "DOCKER_REPOSITORY" | awk '{print $2}'`
-    if [[ -z $docker_registry_host ||
-        -z $docker_repository ]]; then
-       echo "Docker Registry Host and Repository are required!"
-       exit 1
-    fi
-
-    export FULL_DOCKER_REPOSITORY=$docker_registry_host/$docker_repository
-
     if [[ -z $1 ]]; then
        echo "DOCKER_TAG is required to build a container"
        exit 1
@@ -117,35 +107,82 @@ major_version=`cat DOCKER_METADATA | grep "MAJOR_VERSION" | awk '{print $2}'`
 if [[ "$TRAVIS_PULL_REQUEST" == "false" &&
      "$TRAVIS_EVENT_TYPE" == "push" ]]; then
 
-    #increment git version
     echo "Current Major Version is $major_version"
+    if [[ "$DOCKER_GIT_TAG_ENABLED" == "true" ]]; then
 
-    git fetch --tags
-    git_tags=`git tag -l --sort=v:refname | grep $major_version || echo ""`
-    current_minor_version=""
-    for git_tag in $git_tags; do
-        current_minor_version=$git_tag
-    done
+        echo "Using Git Tags to increment version"
 
-    if [[ ! -z $current_minor_version ]]; then
-        echo "Current Minor Version is $current_minor_version"
-        version_parts=( ${current_minor_version//./ } )
-        version_parts[1]=$(( ${version_parts[1]}+1 ))
-        new_minor_version=${version_parts[0]}.${version_parts[1]}
+        #increment git version
+        git fetch --tags
+        git_tags=`git tag -l --sort=v:refname | grep $major_version || echo ""`
+        current_minor_version=""
+        for git_tag in $git_tags; do
+            current_minor_version=$git_tag
+        done
+
+        if [[ ! -z $current_minor_version ]]; then
+            echo "Current Minor Version is $current_minor_version"
+            version_parts=( ${current_minor_version//./ } )
+            version_parts[1]=$(( ${version_parts[1]}+1 ))
+            new_minor_version=${version_parts[0]}.${version_parts[1]}
+        else
+            echo "No Current Minor Version"
+            new_minor_version=$major_version"0"
+        fi
     else
-        echo "No Current Minor Version"
-        new_minor_version=$major_version"0"
+
+        echo "Using Docker Tags to increment version"
+        docker_registry_host=`cat DOCKER_METADATA | grep "DOCKER_REGISTRY_HOST" | awk '{print $2}'`
+        docker_repository=`cat DOCKER_METADATA | grep "DOCKER_REPOSITORY" | awk '{print $2}'`
+        if [[ -z $docker_registry_host ||
+            -z $docker_repository ]]; then
+           echo "Docker Registry Host and Repository are required!"
+           exit 1
+        fi
+    
+        export FULL_DOCKER_REPOSITORY=$docker_registry_host/$docker_repository
+        tag_catalog_url=`echo $docker_repository | awk -v host=$docker_registry_host -F'/' '{print "https://"host"/v2/"$1"/"$2"/tags/list"}'`
+
+        echo "Fetching base image tags at: [ $tag_catalog_url ]"
+        major_version_tags=`curl -u $DOCKER_BUILDER_USER:$DOCKER_BUILDER_PASSWORD $tag_catalog_url | jq -r '.tags[]' | grep $major_version || echo ""`
+        if [[ ! -z $major_version_tags ]]; then
+            echo "Tags from [ $docker_registry_host/$docker_repository ] that match desired major version [ $major_version ]"
+            echo $major_version_tags
+            minor_version=-1
+
+            for tag in $major_version_tags; do
+                echo $tag
+                tag_minor_version=$(echo $tag | awk -F'.' '{print $2}')
+                echo $tag_minor_version
+                if (( $tag_minor_version > $minor_version )); then
+                    minor_version=$tag_minor_version
+                fi
+            done
+            if (( $minor_version > -1 )); then
+               (($minor_version++))
+            else
+               $minor_version=0
+            fi
+            new_minor_version=$major_version$minor_version
+        else
+            echo "No Major Version tags found!"
+            new_minor_version=$major_version"0"
+        fi
     fi
 
     echo "New Minor Version is $new_minor_version"
     build_container $new_minor_version.$TRAVIS_BUILD_NUMBER
-    git tag -a $new_minor_version -m "Tag for version $new_minor_version"
-    git push origin tag $new_minor_version -f
-    
+
+    if [[ "$DOCKER_GIT_TAG_ENABLED" == "true" ]]; then
+        git tag -a $new_minor_version -m "Tag for version $new_minor_version"
+        git push origin tag $new_minor_version -f
+    fi
+
 elif [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
     build_container `echo $TRAVIS_PULL_REQUEST_BRANCH | awk '{ gsub("/", "-"); print }'`
 
-elif [[ "$TRAVIS_EVENT_TYPE" == "api" ]]; then
+elif [[ "$TRAVIS_EVENT_TYPE" == "api" &&
+    "$DOCKER_GIT_TAG_ENABLED" ]]; then
 
     echo "Triggered by API. Looping through known tags and updating..."
     git fetch --tags
