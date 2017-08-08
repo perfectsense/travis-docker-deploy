9edbe54881cc244e08e7c53fa541992a738f1f4c
Todd Loisel <todd@perfectsensedigital.com>
2017-08-08 13:59:42 -0400

    Use docker_metadata.sh source to retrieve docker metadata variables

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -95,13 +95,13 @@ function build_container() {
 
 function calculate_tags_and_build_container() {
 
-    echo "Current Major Version is $major_version"
+    echo "Current Major Version is $MAJOR_VERSION"
     if [[ "$DOCKER_GIT_TAG_ENABLED" == "true" ]]; then
 
         echo "Using Git Tags to increment version"
 
         git fetch --tags
-        git_tags=`git tag -l --sort=v:refname | grep $major_version || echo ""`
+        git_tags=`git tag -l --sort=v:refname | grep $MAJOR_VERSION || echo ""`
         current_minor_version=""
         for git_tag in $git_tags; do
             current_minor_version=$git_tag
@@ -114,7 +114,7 @@ function calculate_tags_and_build_container() {
             new_minor_version=${version_parts[0]}.${version_parts[1]}
         else
             echo "No Current Minor Version"
-            new_minor_version=$major_version"0"
+            new_minor_version=$MAJOR_VERSION"0"
         fi
     else
 
@@ -122,13 +122,13 @@ function calculate_tags_and_build_container() {
         tag_catalog_url=`echo $DOCKER_REPOSITORY | awk -v host=$DOCKER_REGISTRY_HOST -F'/' '{print "https://"host"/v2/"$1"/"$2"/tags/list"}'`
 
         echo "Fetching base image tags at: [ $tag_catalog_url ]"
-        major_version_tags=`curl -u $DOCKER_BUILDER_USER:$DOCKER_BUILDER_PASSWORD $tag_catalog_url | jq -r '.tags[]' | grep $major_version || echo ""`
+        major_version_tags=`curl -u $DOCKER_BUILDER_USER:$DOCKER_BUILDER_PASSWORD $tag_catalog_url | jq -r '.tags[]' | grep $MAJOR_VERSION || echo ""`
         if [[ ! -z $major_version_tags ]]; then
-            echo "Tags from [ $DOCKER_REGISTRY_HOST/$DOCKER_REPOSITORY ] that match desired major version [ $major_version ]"
-            echo $major_version_tags
+            echo "Tags from [ $DOCKER_REGISTRY_HOST/$DOCKER_REPOSITORY ] that match desired major version [ $MAJOR_VERSION ]"
+            echo $MAJOR_VERSION_tags
             minor_version=-1
 
-            for tag in $major_version_tags; do
+            for tag in $MAJOR_VERSION_tags; do
                 tag_minor_version=$(echo $tag | awk -F'.' '{print $2}')
                 if (( $tag_minor_version > $minor_version )); then
                     minor_version=$tag_minor_version
@@ -139,10 +139,10 @@ function calculate_tags_and_build_container() {
             else
                $minor_version=0
             fi
-            new_minor_version=$major_version$minor_version
+            new_minor_version=$MAJOR_VERSION$minor_version
         else
             echo "No Major Version tags found!"
-            new_minor_version=$major_version"0"
+            new_minor_version=$MAJOR_VERSION"0"
         fi
     fi
 
@@ -155,18 +155,19 @@ function calculate_tags_and_build_container() {
     fi
 }
 
-echo "Installing Berkshelf"
-gem install berkshelf
-
 echo "Current Working Directory is [ $(pwd) ]"
 export BUILD_DIRECTORY=$(pwd)
+export CHEF_DIRECTORY=$(pwd)/../../chef
 
 set -e -u
 
-major_version=`cat DOCKER_METADATA | grep "MAJOR_VERSION" | awk '{print $2}'`
-export DOCKER_REGISTRY_HOST=`cat DOCKER_METADATA | grep "DOCKER_REGISTRY_HOST" | awk '{print $2}'`
-export DOCKER_REPOSITORY=`cat DOCKER_METADATA | grep "DOCKER_REPOSITORY" | awk '{print $2}'`
-export DOCKER_GIT_TAG_ENABLED=`cat DOCKER_METADATA | grep "DOCKER_GIT_TAG_ENABLED" | awk '{print $2}'`
+if [[ ! -f docker_metadata.sh ]]; then
+    echo "docker_metadata file not found!"
+    exit 1
+fi
+
+. ./docker_metadata.sh
+
 if [[ -z $DOCKER_REGISTRY_HOST ||
     -z $DOCKER_REPOSITORY ]]; then
    echo "Docker Registry Host and Repository are required!"
@@ -178,8 +179,8 @@ export FULL_DOCKER_REPOSITORY=$DOCKER_REGISTRY_HOST/$DOCKER_REPOSITORY
 if [[ "$TRAVIS_PULL_REQUEST" == "false" &&
     "$TRAVIS_EVENT_TYPE" == "push" ]]; then    
 
-    environments_enabled=`cat DOCKER_METADATA | grep "ENVIRONMENTS_ENABLED" | awk '{print $2}'`
-    if [[ $environments_enabled == "true" ]]; then
+    if [[ ${ENVIRONMENTS_ENABLED+x} &&
+        $ENVIRONMENTS_ENABLED == "true" ]]; then
 
         echo "Building Containers for all environments in [$TRAVIS_BUILD_DIR/ops/chef/environments]"
 
